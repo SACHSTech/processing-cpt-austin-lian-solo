6e6e1179e7f5fafa875c0144d0528c49f8127a91
LianAustin <austin.lian25@ycdsbk12.ca>
2024-06-06 18:17:06 -0400

    Fiddled around a bit to get the seperate gif testing file to work

--- Main.java
+++ Main.java
@@ -2,17 +2,18 @@ import processing.core.PApplet;
 
 /**
  * Main class to execute sketch
- * @author 
+ * @author
  *
  */
 class Main {
   public static void main(String[] args) {
-    
+
     String[] processingArgs = {"MySketch"};
-	  Sketch mySketch = new Sketch();  //comment this out to run the other sketch files
+		Sketch mySketch = new Sketch();  //comment this out to run the other sketch files
 	  // Sketch1 mySketch = new Sketch1();  // uncomment this to run this sketch file
 	  // Sketch2 mySketch = new Sketch2();  // uncomment this to run this sketch file
-	  
+	
+
 	  PApplet.runSketch(processingArgs, mySketch);
   }
   
